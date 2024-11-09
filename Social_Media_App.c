aacf1e9f3bcf7478baaa041d3a8caaddd6c20b48
BilaUsmani02 <busmani8460@gmail.com>
2024-11-09 22:08:44 +0500

    login page

--- Social_Media_App.c
+++ Social_Media_App.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
 int main(void){
+    int choice;
     
+    printf("-Login      = 1\n");
+    printf("-Register   = 2\n");
+    printf("Choose an option: \n");
+    
+    switch (choice)
+    {
+    case 1:
+        login();
+        break;
+    
+    case 2:
+        reg();
+        break;
+
+    default:
+        printf("Choose a correct option")
+        break;
+    }
+
 }
